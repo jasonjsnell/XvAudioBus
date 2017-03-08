b160dc821d37c5e27678e06cc012f74bf322b638
jasonjsnell <snell.jason@gmail>
2017-03-08 07:17:01 -0800

    Initial Commit

---
+++ XvAudioBus/XvAudioBus.h
@@ -0,0 +1,19 @@
+//
+//  XvAudioBus.h
+//  XvAudioBus
+//
+//  Created by Jason Snell on 3/8/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvAudioBus.
+FOUNDATION_EXPORT double XvAudioBusVersionNumber;
+
+//! Project version string for XvAudioBus.
+FOUNDATION_EXPORT const unsigned char XvAudioBusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvAudioBus/PublicHeader.h>
+
+
